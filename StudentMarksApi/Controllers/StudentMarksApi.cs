de4e0b96d3e5c539e2526ab92bac782da6701727
agng2022 <125799355+agng2022@users.noreply.github.com>
2025-01-20 22:23:11 +0530

    Initial commit

---
+++ StudentMarksApi/Controllers/StudentMarksApi.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentMarksApi.Models;
+using System.Collections.Generic;
+
+namespace StudentMarksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudentController(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("GetStudents")]
+        public IActionResult GetStudents()
+        {
+            var students = new List<Student>
+            {
+                new Student { Id = 1, Name = "John Doe", SubjectMarks = new Dictionary<string, int> { { "Math", 85 }, { "Science", 90 } } },
+                new Student { Id = 2, Name = "Jane Smith", SubjectMarks = new Dictionary<string, int> { { "Math", 78 }, { "Science", 88 } } }
+            };
+
+            return Ok(students);
+        }
+
+        [HttpGet("GetConfig")]
+        public IActionResult GetConfig()
+        {
+            var configData = new
+            {
+                Environment = _configuration["Environment"],
+                ApplicationName = _configuration["ApplicationName"],
+                Version = _configuration["Version"]
+            };
+
+            return Ok(configData);
+        }
+    }
+}
---
+++ StudentMarksApi/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentMarksApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Dictionary<string, int> SubjectMarks { get; set; } = new();
+    }
+}
